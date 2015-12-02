81c1e7b3ce8492561f1b0e914c4fb8c287be0e63
Pascal Hüntemann <pascal_huentemann@t-online.de>
2015-12-02 21:01:00 +0100

    changed paths to use file related paths

--- game_change_notifier.py
+++ game_change_notifier.py
@@ -10,14 +10,19 @@ import sys
 import os
 import gc
 
+DEBUG = True
+PATH = os.path.dirname(os.path.abspath(__file__))
+
 class GameChanger(object):
 	"""Check for a game change of a twitch streamer"""
 	def __init__(self):
-		logging.basicConfig(filename='log.txt', level=logging.DEBUG,
+		logging.basicConfig(filename=os.path.join(PATH, 'log.txt'), level=logging.DEBUG,
 							format='%(asctime)s - %(levelname)s - %(message)s')
 		api_key, client_id, oauth, streamers = self.get_config()
 		self.init_db()
 		self.pbullet = Pushbullet(api_key)
+		if DEBUG:
+			self.pbullet.push_note("Test", "This is a debug message.")
 		for streamer in streamers:
 			### Url to Twitch API (see https://github.com/justintv/Twitch-API) ###
 			url = "https://api.twitch.tv/kraken/streams/{0}".format(streamer)
@@ -26,8 +31,8 @@ class GameChanger(object):
 	### Get the config data from "config.cfg" file in same folder ###
 	def get_config(self):
 		"""Get config settings from config.cfg"""
-		if os.path.isfile("config.cfg"):
-			config_data = json.load(open("config.cfg"))
+		if os.path.isfile(os.path.join(PATH, "config.cfg")):
+			config_data = json.load(open(os.path.join(PATH, "config.cfg")))
 			api_key = config_data["api_key"]
 			client_id = config_data["client_id"]
 			oauth = config_data["oauth"]
@@ -40,16 +45,16 @@ class GameChanger(object):
 				"oauth": "",
 				"streamers": []
 			}
-			json.dump(cfg, open("config.cfg", "w"))
+			json.dump(cfg, open(os.path.join(PATH, "config.cfg"), "w"))
 			logging.warning("Please update the config.cfg!")
 			sys.exit()
 
 	### Initialize the database if not yet initiated ###
 	def init_db(self):
 		"""Initialize the database if not already done"""
-		if not os.path.isfile("games.db"):
+		if not os.path.isfile(os.path.join(PATH, "games.db")):
 			logging.debug("Initiating database.")
-			conn = sqlite3.connect("games.db")
+			conn = sqlite3.connect(os.path.join(PATH, "games.db"))
 			cursor = conn.cursor()
 			cursor.execute("""CREATE TABLE games (streamer TEXT PRIMARY KEY,
 					  game TEXT, last_changed TEXT)""")
@@ -62,7 +67,7 @@ class GameChanger(object):
 	### Check the database for the streamer and game ###
 	def check_db(self, streamer, game):
 		"""Check the database entry for a given streamer"""
-		conn = sqlite3.connect("games.db")
+		conn = sqlite3.connect(os.path.join(PATH, "games.db"))
 		cursor = conn.cursor()
 		cursor.execute("""SELECT * FROM games WHERE streamer=?""", (streamer,))
 		result = cursor.fetchone() #returns as a tuple e.g.(u'lirik', u'Arma3', u'2015-12-02 18:30')
@@ -95,7 +100,7 @@ class GameChanger(object):
 		data = requests.get(url, headers=headers)
 		### Check the responses status code and act accordingly ###
 		if data.status_code != 200:
-			json.dump(data.json(), open("bad_response.json", "w"))
+			json.dump(data.json(), open(os.path.join(PATH, "bad_response.json"), "w"))
 			logging.warning("Bad response code: {0}\nCheck bad_response.json".format(data.status_code))
 			sys.exit()
 		else:
